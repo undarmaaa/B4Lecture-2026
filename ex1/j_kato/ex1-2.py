cb13417d9e6d5828f70a3d55ebb5513ed09b557b
j_kato <j_kato@aventador.sp.m.is.nagoya-u.ac.jp>
2026-05-08 17:18:15 +0900

    課題完成-1

--- ex1/j_kato/ex1-2.py
+++ ex1/j_kato/ex1-2.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 import numpy
 import pandas as pd
 
-
+#トラッキングデータのデータパス
 data_path = "/mnt/datapool00/laliga_23/skillcorner_v2/tracking/1018887.json"
+#選手情報を含んだファイルのデータパス
 player_data_path = "/mnt/datapool00/laliga_23/skillcorner_v2/metadata/players.json"
 
 with open(data_path, "r") as f:
@@ -13,6 +14,7 @@ with open(data_path, "r") as f:
 with open(player_data_path, "r") as f:
     player_data = json.load(f)
 
+#player_id=246の選手に対応するtrackable_object を取得
 for p in player_data:
     if p["id"]==246:
         target_track_id = p["trackable_object"]
@@ -21,11 +23,12 @@ px=[]
 py=[]
 times=[]
 
+#0から500フレームのデータを取得する
 for frame_idx in range(500):
     frame=data[frame_idx]
     objects=frame["data"]
 
-
+    #現在のフレーム内の全オブジェクトあら対象選手の座標を取得する
     for obj in objects:
         if obj["trackable_object"]==target_track_id:
             px.append(obj["x"])
@@ -33,27 +36,36 @@ for frame_idx in range(500):
             times.append(frame_idx)
 
 speed=[]
+#隣接フレーム間の移動距離から速度を計算
 for i in range(1,len(px)):
+    #x方向・y方向の移動量
     dx=px[i]-px[i-1]
     dy=py[i]-py[i-1]
 
+    #ユーグリッド距離を計算
     distance=(dx**2+dy**2)**0.5
+    #速度を計算
     v=distance/0.1
-
 
     speed.append(v)
 
 acceleration=[]
-
+#隣接時刻の速度変化から加速度を計算
 for i in range(1,len(speed)):
+    #速度差を計算
     dv=speed[i]-speed[i-1]
+    #加速度を計算
     a=dv/0.1
 
     acceleration.append(a)
 
+# 縦2つのグラフを作成
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8))
 
+#速度・加速度に対応する時間軸を作成
+#速度は1フレーム差分なので times[1:]
 time_speed = [t*0.1 for t in times[1:]]
+#加速度は2フレーム差分なので times[2:]
 time_acc = [t*0.1 for t in times[2:]]
 
 ax1.plot(time_speed, speed)
--- ex1/j_kato/ex1-3.py
+++ ex1/j_kato/ex1-3.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import pandas as pd
 
-
+#トラッキングデータのデータパス
 data_path = "/mnt/datapool00/laliga_23/skillcorner_v2/tracking/1018887.json"
 
+#チーム情報を含んだファイルのデータパス
 player_team_path = "/mnt/datapool00/laliga_23/skillcorner_v2/match_test/1018887.json"
 
 with open(data_path, "r") as f:
@@ -14,41 +15,51 @@ with open(data_path, "r") as f:
 with open(player_team_path, "r") as f:
     team_data = json.load(f)
 
+#描画用のfigureとaxesを作成
 fig, ax = plt.subplots()
 
-def draw_pitch(ax):
+#フィールドの縦・横の長さを取得
+pitch_length = team_data["pitch_length"]
+pitch_width = team_data["pitch_width"]
+
+#サッカーフィールドをグラフ上に描画する関数
+def draw_field(ax):
+    #軸のメモリを削除する
     ax.set_xticks([])
     ax.set_yticks([])
+    #軸のラベルを削除する
     ax.set_xlabel("")
     ax.set_ylabel("")
 
+    #グラフの外枠を非表示にする
     for spine in ax.spines.values():
         spine.set_visible(False)
     
-    ax.plot([-52.5, 52.5], [-34, -34], color="black")
-    ax.plot([-52.5, 52.5], [34, 34], color="black")
-    ax.plot([-52.5, -52.5], [-34, 34], color="black")
-    ax.plot([52.5, 52.5], [-34, 34], color="black")
+    #サッカーフィールドの外枠を描画
+    ax.plot([-pitch_length/2, pitch_length/2],[-pitch_width/2, -pitch_width/2],color="black")
+    ax.plot([-pitch_length/2, pitch_length/2],[pitch_width/2, pitch_width/2],color="black")
+    ax.plot([-pitch_length/2, -pitch_length/2],[-pitch_width/2, pitch_width/2],color="black")
+    ax.plot([pitch_length/2, pitch_length/2],[-pitch_width/2, pitch_width/2],color="black")
 
-    ax.plot([0, 0], [-34, 34], color="black")
+    #サッカーフィールドのセンターラインを描画
+    ax.plot([0, 0], [-pitch_width/2, pitch_width/2], color="black")
 
+    #サッカーフィールドのセンターサークルを描画
     circle = plt.Circle((0, 0), 9.15, fill=False, color="black")
     ax.add_patch(circle)
 
-    # 左
+    #サッカーフィールドの左ペナルティエリアを描画
     ax.plot([-52.5, -36], [-20.16, -20.16], color="black")
     ax.plot([-52.5, -36], [20.16, 20.16], color="black")
     ax.plot([-36, -36], [-20.16, 20.16], color="black")
 
-    # 右
+    #サッカーフィールドの右ペナルティエリアを描画
     ax.plot([52.5, 36], [-20.16, -20.16], color="black")
     ax.plot([52.5, 36], [20.16, 20.16], color="black")
     ax.plot([36, 36], [-20.16, 20.16], color="black")
 
+    #背景を芝生色に設定
     ax.set_facecolor("#6CAB64")  
-
-
-
 
 
 
@@ -57,18 +68,25 @@ track_to_team = {}
 for p in team_data["players"]:
     track_to_team[p["trackable_object"]] = p["team_id"]
 
+#ホームチームとアウェイチームのIDをそれぞれ取得する
 home_id = team_data["home_team"]["id"]
 away_id = team_data["away_team"]["id"]
 
+#ホームチームとアウェイチームのユニフォームカラーをそれぞれ取得する
 home_color = team_data["home_team_kit"]["jersey_color"]
 away_color = team_data["away_team_kit"]["jersey_color"]
 
+#アニメーションを更新する関数
 def update(frame_idx):
+    #前フレームを消去
     ax.clear()
-    draw_pitch(ax)
+    #フィールド描画
+    draw_field(ax)
 
+    #現在フレームの取得
     frame=data[frame_idx]
 
+    #データが存在しない場合は終了する
     if len(frame["data"])==0:
         return
 
@@ -77,8 +95,9 @@ def update(frame_idx):
     players=[]
     ball=[]
 
+    #ボールと選手を分類する
     for obj in objects:
-        if "z" in obj:
+        if "z" in obj: #z座標が存在するものをボールとして扱う
             ball.append(obj)
         else:
             players.append(obj)
@@ -90,6 +109,7 @@ def update(frame_idx):
     trail_x = []
     trail_y = []
 
+    #5フレーム前までのボールの座標を取得する
     for i in range(max(0, frame_idx-5), frame_idx+1):
         frame = data[i]
 
@@ -105,10 +125,12 @@ def update(frame_idx):
     px_away, py_away = [], []
 
 
+    #選手ごとにチーム分類
     for obj in players:
         tid = obj["trackable_object"]
 
         if tid in track_to_team:
+            #対応辞書から選手のチームIDを取得
             team_id = track_to_team[tid]
 
             if team_id == home_id:
@@ -118,13 +140,17 @@ def update(frame_idx):
                 px_away.append(obj["x"])
                 py_away.append(obj["y"])
 
+    #ホーム/アウェイそれぞれのチームの選手を描画
     ax.scatter(px_home, py_home, c=home_color, label="Home")
     ax.scatter(px_away, py_away, c=away_color, label="Away")
+    #5フレーム前までのボールの軌跡を描画
     ax.scatter(trail_x, trail_y, c="white", alpha=0.3, s=30) 
+    #ボールを描画
     ax.scatter(bx, by,  c="white", edgecolors="black", s=100,label="Ball")
 
-    ax.set_xlim(-60,60)
-    ax.set_ylim(-40,40)
+    #描画範囲を設定
+    ax.set_xlim(-pitch_length/2 - 5, pitch_length/2 + 5) 
+    ax.set_ylim(-pitch_width/2 - 5, pitch_width/2 + 5)
 
     ax.legend(loc="lower center", bbox_to_anchor=(0.5, -0.1), ncol=3)
 
@@ -132,6 +158,8 @@ def update(frame_idx):
 
     ax.set_title(f"Frame {frame_idx}")
 
+#アニメーションを作成
 ani = FuncAnimation(fig, update, frames=200, interval=50)
 
+#GIFでアニメーションを保存
 ani.save("BallTrajectoryAnimation.gif", writer="pillow")
